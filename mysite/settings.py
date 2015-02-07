ec6852b75d1e31ac8ab8bd4e54c7d0eb5caf461a
Gosia <gosia@gmail.com>
2015-02-07 16:47:46 +0100

    Moja aplikacja Django Girls

--- mysite/settings.py
+++ mysite/settings.py
@@ -69,16 +69,31 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+import dj_database_url
+DATABASES['default'] = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+	from .local_settings import *
+except ImportError:
+	pass
